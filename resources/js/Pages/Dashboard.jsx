d6150259b5f652cd8ccad76fb0a33a2cf0e434e7
Julia Ohorodnichuk <ogorodnicukulia@gmail.com>
2024-09-14 11:55:31 +0300

    feat: added Index component with AuthenticatedLayout for displaying projects

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -6,7 +6,7 @@ export default function Dashboard() {
         <AuthenticatedLayout
             header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
         >
-            <Head title="Dashboard" />
+            <Head title="Projects" />
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
