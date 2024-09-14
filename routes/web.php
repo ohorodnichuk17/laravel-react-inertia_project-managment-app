dcd0c22b45d6243d586e2ce75bfe84cf863d9fc7
Julia Ohorodnichuk <ogorodnicukulia@gmail.com>
2024-09-14 11:25:15 +0300

    feat: created user, task and project controllers & modified web.php to add routes

--- routes/web.php
+++ routes/web.php
@@ -5,18 +5,16 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+Route::redirect('/', '/dashboard');
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/dashboard', fn() => Inertia::render('Dashboard'))
+    ->name('dashboard');
+
+    Route::resource('project', \App\Http\Controllers\ProjectController::class);
+    Route::resource('task', \App\Http\Controllers\TaskController::class);
+    Route::resource('user', \App\Http\Controllers\UserController::class);
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
