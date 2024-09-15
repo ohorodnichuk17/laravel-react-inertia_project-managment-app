52adfe804ce8ce9b541ec216064681e0c356992b
Julia Ohorodnichuk <ogorodnicukulia@gmail.com>
2024-09-15 09:51:13 +0300

    feat: added search functionality with text and select inputs

--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -1,12 +1,30 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.jsx";
-import {Link} from "@inertiajs/react";
+import {Link, router} from "@inertiajs/react";
 import Pagination from "@/Components/Pagination.jsx";
 import {PROJECT_STATUS_CLASS_MAP, PROJECT_STATUS_TEXT_MAP} from "@/constants.jsx";
 import TextInput from "@/Components/TextInput.jsx";
 import SelectInput from "@/Components/SelectInput.jsx";
 
 
-export default function Index({auth, projects}) {
+export default function Index({auth, projects, queryParams = null}) {
+  queryParams = queryParams || {};
+
+  const searchFieldChanged = (name, value) => {
+    if(value) {
+      queryParams[name] = value;
+    } else {
+      delete queryParams[name];
+    }
+
+    router.get(route('project.index'), queryParams);
+  }
+
+  const onKeyPress = (name, e) => {
+    if(e.key !== 'Enter') return;
+
+    searchFieldChanged(name, e.target.value);
+  }
+
   return (
     <AuthenticatedLayout
       user={auth.user}
@@ -27,10 +45,23 @@ export default function Index({auth, projects}) {
                   <th className="px-3 py-3"></th>
                   <th className="px-3 py-3"></th>
                   <th className="px-3 py-3">
-                    <TextInput />
+                    <TextInput
+                      className="w-full"
+                      placeholder="Project Name"
+                      onBlur={e => searchFieldChanged('name', e.target.value)}
+                      onKeyPress={e => onKeyPress('name', e)}
+                    />
                   </th>
                   <th className="px-3 py-3">
-                    <SelectInput/>
+                    <SelectInput
+                      className="w-full"
+                      onChange={e => searchFieldChanged('status', e.target.value)}
+                    >
+                      <option value="">Select Status</option>
+                      <option value="pending">Pending</option>
+                      <option value="in_progress">In Progress</option>
+                      <option value="completed">Completed</option>
+                    </SelectInput>
                   </th>
                   <th className="px-3 py-3"></th>
                   <th className="px-3 py-3"></th>
