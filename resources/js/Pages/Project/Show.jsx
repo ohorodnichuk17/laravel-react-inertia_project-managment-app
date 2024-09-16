bf7d6b0bb15c04123e64a7d57ef234f6526479fd
Julia Ohorodnichuk <ogorodnicukulia@gmail.com>
2024-09-16 16:47:32 +0300

    feat: display detailed project info with image, status, and metadata on show page

--- resources/js/Pages/Project/Show.jsx
+++ resources/js/Pages/Project/Show.jsx
@@ -1,5 +1,6 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.jsx";
 import { Head } from "@inertiajs/react";
+import {PROJECT_STATUS_CLASS_MAP, PROJECT_STATUS_TEXT_MAP} from "@/constants.jsx";
 
 export default function Show({ auth, project }) {
   return (
@@ -16,8 +17,62 @@ export default function Show({ auth, project }) {
       <div className="py-12">
         <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
           <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-            <div className="p-6 text-gray-900 dark:text-gray-100">
-              Show
+            <div className="p-6 text-gray-900 dark:text-gray-400">
+              <div>
+                <img
+                  src={project.image_path}
+                  alt=""
+                  className="w-full h-64 object-cover"
+                />
+              </div>
+              <div className="grid gap-1 grid-cols-2 mt-2">
+                <div>
+                  <div>
+                    <label className="font-bold text-lg">Project ID</label>
+                    <p className="mt-1">{project.id}</p>
+                  </div>
+                  <div className="mt-4">
+                    <label className="font-bold text-lg">Project Name</label>
+                    <p className="mt-1">{project.name}</p>
+                  </div>
+                  <div className="mt-4">
+                    <label className="font-bold text-lg">Project Status</label>
+                    <p className="mt-1">
+                      <span className={
+                        "px-2 py-1 rounded text-white " +
+                        PROJECT_STATUS_CLASS_MAP[project.status]
+                      }>
+                        {PROJECT_STATUS_TEXT_MAP[project.status]}
+                      </span>
+                    </p>
+                  </div>
+                  <div className="mt-4">
+                    <label className="font-bold text-lg">Created By</label>
+                    <p className="mt-1">{project.createdBy.name}</p>
+                  </div>
+                </div>
+
+                <div>
+                  <div>
+                    <label className="font-bold text-lg">Due Date</label>
+                    <p className="mt-1">{project.due_date}</p>
+                  </div>
+                  <div className="mt-4">
+                    <label className="font-bold text-lg">Create Date</label>
+                    <p className="mt-1">{project.created_at}</p>
+                  </div>
+                  <div className="mt-4">
+                    <label className="font-bold text-lg">Updated By</label>
+                    <p className="mt-1">{project.updatedBy.name}</p>
+                  </div>
+                </div>
+
+
+              </div>
+              <div className="mt-4">
+                <label className="font-bold text-lg">Project Description</label>
+                <p className="mt-1">{project.description}</p>
+              </div>
             </div>
           </div>
         </div>
