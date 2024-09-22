76856581759621dd19d76e3db6f0cb479b01544e
Julia Ohorodnichuk <ogorodnicukulia@gmail.com>
2024-09-22 18:43:17 +0300

    feat: outputted project name when deletion succeded

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -109,7 +109,8 @@ class ProjectController extends Controller
      */
     public function destroy(Project $project)
     {
+        $name = $project->name;
         $project->delete();
-        return to_route('project.index')->with('success', 'Project was deleted');
+        return to_route('project.index')->with('success', "Project {$name} was deleted");
     }
 }
