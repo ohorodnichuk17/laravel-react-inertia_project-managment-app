76f2b2e097d440e10b83cc59b4da6b1f560a4c8a
Julia Ohorodnichuk <ogorodnicukulia@gmail.com>
2024-09-26 13:07:51 +0300

    feat: full CRUD functionality for tasks section

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -9,6 +9,19 @@ class Task extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'name',
+        'description',
+        'image_path',
+        'status',
+        'priority',
+        'due_date',
+        'assigned_user_id',
+        'created_by',
+        'updated_by',
+        'project_id',
+    ];
+
     public function project()
     {
         return $this->belongsTo(Project::class);
@@ -16,17 +29,16 @@ class Task extends Model
 
     public function assignedUser()
     {
-        return $this->belongsTo(User::class, "assigned_user_id");
-
+        return $this->belongsTo(User::class, 'assigned_user_id');
     }
 
     public function createdBy()
     {
-        return $this->belongsTo(User::class, "created_by");
+        return $this->belongsTo(User::class, 'created_by');
     }
 
     public function updatedBy()
     {
-        return $this->belongsTo(User::class, "updated_by");
+        return $this->belongsTo(User::class, 'updated_by');
     }
 }
--- resources/js/Pages/Task/TasksTable.jsx
+++ resources/js/Pages/Task/TasksTable.jsx
@@ -1,49 +1,64 @@
-import TableHeading from "@/Components/TableHeading.jsx";
-import TextInput from "@/Components/TextInput.jsx";
-import SelectInput from "@/Components/SelectInput.jsx";
-import {TASK_STATUS_CLASS_MAP, TASK_STATUS_TEXT_MAP} from "@/constants.jsx";
-import {Link, router} from "@inertiajs/react";
-import Pagination from "@/Components/Pagination.jsx";
+import Pagination from "@/Components/Pagination";
+import SelectInput from "@/Components/SelectInput";
+import TextInput from "@/Components/TextInput";
+import TableHeading from "@/Components/TableHeading";
+import { TASK_STATUS_CLASS_MAP, TASK_STATUS_TEXT_MAP } from "@/constants.jsx";
+import { Link, router } from "@inertiajs/react";
 
-export default function TasksTable({tasks, queryParams = null, hideProjectColumn = false}) {
+export default function TasksTable({
+                                     tasks,
+                                     success,
+                                     queryParams = null,
+                                     hideProjectColumn = false,
+                                   }) {
   queryParams = queryParams || {};
-
   const searchFieldChanged = (name, value) => {
-    if(value) {
+    if (value) {
       queryParams[name] = value;
     } else {
       delete queryParams[name];
     }
 
-    router.get(route('task.index'), queryParams);
-  }
+    router.get(route("task.index"), queryParams);
+  };
 
   const onKeyPress = (name, e) => {
-    if(e.key !== 'Enter') return;
+    if (e.key !== "Enter") return;
 
     searchFieldChanged(name, e.target.value);
-  }
+  };
 
   const sortChanged = (name) => {
-    if(name === queryParams.sort_field) {
-      if(queryParams.sort_direction === 'asc') {
-        queryParams.sort_direction = 'desc';
+    if (name === queryParams.sort_field) {
+      if (queryParams.sort_direction === "asc") {
+        queryParams.sort_direction = "desc";
       } else {
-        queryParams.sort_direction = 'asc';
+        queryParams.sort_direction = "asc";
       }
     } else {
       queryParams.sort_field = name;
-      queryParams.sort_direction = 'asc';
+      queryParams.sort_direction = "asc";
     }
-    router.get(route('task.index'), queryParams);
+    router.get(route("task.index"), queryParams);
+  };
+
+  const deleteTask = (task) => {
+    if (!window.confirm("Are you sure you want to delete the task?")) {
+      return;
+    }
+    router.delete(route("task.destroy", task.id));
   };
 
   return (
     <>
+      {success && (
+        <div className="bg-emerald-500 py-2 px-4 text-white rounded mb-4">
+          {success}
+        </div>
+      )}
       <div className="overflow-auto">
         <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-          <thead
-            className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500">
           <tr className="text-nowrap">
             <TableHeading
               name="id"
@@ -65,6 +80,7 @@ export default function TasksTable({tasks, queryParams = null, hideProjectColumn
             >
               Name
             </TableHeading>
+
             <TableHeading
               name="status"
               sort_field={queryParams.sort_field}
@@ -73,14 +89,16 @@ export default function TasksTable({tasks, queryParams = null, hideProjectColumn
             >
               Status
             </TableHeading>
+
             <TableHeading
               name="created_at"
               sort_field={queryParams.sort_field}
               sort_direction={queryParams.sort_direction}
               sortChanged={sortChanged}
             >
-              Created at
+              Create Date
             </TableHeading>
+
             <TableHeading
               name="due_date"
               sort_field={queryParams.sort_field}
@@ -90,31 +108,28 @@ export default function TasksTable({tasks, queryParams = null, hideProjectColumn
               Due Date
             </TableHeading>
             <th className="px-3 py-3">Created By</th>
-            <th className="px-3 py-3">Actions</th>
+            <th className="px-3 py-3 text-right">Actions</th>
           </tr>
           </thead>
-          <thead
-            className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500">
           <tr className="text-nowrap">
             <th className="px-3 py-3"></th>
             <th className="px-3 py-3"></th>
-            {!hideProjectColumn && (
-              <th className="px-3 py-3"></th>
-            )}
+            {!hideProjectColumn && <th className="px-3 py-3"></th>}
             <th className="px-3 py-3">
               <TextInput
                 className="w-full"
                 defaultValue={queryParams.name}
                 placeholder="Task Name"
-                onBlur={e => searchFieldChanged('name', e.target.value)}
-                onKeyPress={e => onKeyPress('name', e)}
+                onBlur={(e) => searchFieldChanged("name", e.target.value)}
+                onKeyPress={(e) => onKeyPress("name", e)}
               />
             </th>
             <th className="px-3 py-3">
               <SelectInput
-                defaultValue={queryParams.status}
                 className="w-full"
-                onChange={e => searchFieldChanged('status', e.target.value)}
+                defaultValue={queryParams.status}
+                onChange={(e) => searchFieldChanged("status", e.target.value)}
               >
                 <option value="">Select Status</option>
                 <option value="pending">Pending</option>
@@ -125,55 +140,58 @@ export default function TasksTable({tasks, queryParams = null, hideProjectColumn
             <th className="px-3 py-3"></th>
             <th className="px-3 py-3"></th>
             <th className="px-3 py-3"></th>
-            <th className="px-3 py-3 text-right"></th>
+            <th className="px-3 py-3"></th>
           </tr>
           </thead>
           <tbody>
-          {tasks.data.map(task => (
+          {tasks.data.map((task) => (
             <tr
               className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
               key={task.id}
             >
               <td className="px-3 py-2">{task.id}</td>
               <td className="px-3 py-2">
-                <img src={task.image_path} style={{width: 60}} alt=""/>
+                <img src={task.image_path} style={{ width: 60 }} />
               </td>
               {!hideProjectColumn && (
                 <td className="px-3 py-2">{task.project.name}</td>
               )}
-              <td className="px-3 py-2">{task.name}</td>
+              <th className="px-3 py-2 text-gray-100 hover:underline">
+                <Link href={route("task.show", task.id)}>{task.name}</Link>
+              </th>
               <td className="px-3 py-2">
-                      <span className={
-                        "px-2 py-1 rounded text-white " +
-                        TASK_STATUS_CLASS_MAP[task.status]
-                      }>
-                        {TASK_STATUS_TEXT_MAP[task.status]}
-                      </span>
+                  <span
+                    className={
+                      "px-2 py-1 rounded text-nowrap text-white " +
+                      TASK_STATUS_CLASS_MAP[task.status]
+                    }
+                  >
+                    {TASK_STATUS_TEXT_MAP[task.status]}
+                  </span>
               </td>
               <td className="px-3 py-2 text-nowrap">{task.created_at}</td>
               <td className="px-3 py-2 text-nowrap">{task.due_date}</td>
               <td className="px-3 py-2">{task.createdBy.name}</td>
-              <td className="px-3 py-2">
+              <td className="px-3 py-2 text-nowrap">
                 <Link
-                  href={route('task.edit', task.id)}
-                  className={"font-medium text-blue-600 dark:text-blue-500 hover:underline mx-1"}
+                  href={route("task.edit", task.id)}
+                  className="font-medium text-blue-600 dark:text-blue-500 hover:underline mx-1"
                 >
                   Edit
                 </Link>
-                <Link
-                  href={route('task.destroy', task.id)}
-                  className={"font-medium text-red-600 dark:text-red-500 hover:underline mx-1"}
+                <button
+                  onClick={(e) => deleteTask(task)}
+                  className="font-medium text-red-600 dark:text-red-500 hover:underline mx-1"
                 >
                   Delete
-                </Link>
+                </button>
               </td>
             </tr>
           ))}
           </tbody>
         </table>
       </div>
-
-      <Pagination links={tasks.meta.links}/>
+      <Pagination links={tasks.meta.links} />
     </>
   );
 }
